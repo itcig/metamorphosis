7fc3321e4275266be07748b9c00ef7c294215781
unfrgivn <bradley.ash@gmail.com>
2020-02-06 19:18:57 -0600

    Export all members of database and server adapaters from main app

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@
 import Application, { consumers, producers, services } from './application';
 
 export { default as configuration } from './configuration';
-export { default as databases } from './database-adapters';
-export { default as server } from './server';
+export * from './database-adapters';
+export * from './server';
 
 export * from './types/types';
 
