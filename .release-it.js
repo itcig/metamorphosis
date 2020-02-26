363236dd33b2403e8412a4597c6c4cdb851f38b4
Brad <bradley.ash@gmail.com>
2020-02-26 15:44:03 -0600

    chore: Added build to release hooks

--- .release-it.js
+++ .release-it.js
@@ -1,7 +1,8 @@
 module.exports = {
 	hooks: {
 		// Update changelog and add to publish commit
-		'after:bump': 'npm run changelog',
+		'after:bump': 'npm run build && npm run changelog',
+		'after:release': 'echo Successfully released ${name} v${version} to ${repo.repository}.',
 	},
 	git: {
 		changelog: 'npx auto-changelog -p --commit-limit false --stdout --unreleased --template tpl-release-changes.hbs',
